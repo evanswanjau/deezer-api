b68feb4a14883609df4fb24484a31517b542a5b3
Evans Wanjau <evanswanjau@gmail.com>
2023-05-20 06:39:37 +0300

    update: updated app file 1. Added and configured bodyParser

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,36 @@
 import express from "express";
+import bodyParser from "body-parser";
+
 const app = express();
-const port = 3000;
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, DELETE, OPTIONS"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Systems running successfully!");
 });
+
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port} 🚀 `);
