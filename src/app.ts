e07ce293c8c60439a811bb8a6650c7f05acfdaa3
Evans Wanjau <evanswanjau@gmail.com>
2023-05-21 09:06:43 +0300

    update: added routes to app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import routes from "./routes/index";
 
 const app = express();
 
@@ -29,6 +30,8 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
     res.send("Systems running successfully!");
 });
+
+app.use("/v1", routes);
 
 const port = process.env.PORT || 8000;
 
